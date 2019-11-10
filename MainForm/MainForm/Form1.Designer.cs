a2af6497acedf3072509cb4de6bae87524ed833a
MammadliHuseyn <54790582+MammadliHuseyn@users.noreply.github.com>
2019-11-10 16:56:27 +0400

    Add files via upload

---
+++ MainForm/MainForm/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace MainForm
+{
+    partial class PresentationDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PresentationDemo));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.ColumnWidth = 5;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(807, 60);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(717, 679);
+            this.listBox1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(50, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(207, 558);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "1) ";
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(298, 60);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(478, 558);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "1) ";
+            this.richTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox2_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(87, 653);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 62);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Qarışdır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tələbələr";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(482, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Mövzular";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1077, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nəticə";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(343, 653);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 62);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Təmizlə";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(573, 653);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 62);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Kopyala";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // PresentationDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1557, 750);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PresentationDemo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PresentationDemo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ MainForm/MainForm/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class PresentationDemo : Form
+    {
+        public PresentationDemo()
+        {
+            InitializeComponent();
+        }
+
+        bool _isTrue = false;
+        byte counter = 1;
+        byte counter2 = 1;
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                Random rnd = new Random();
+                int randomNum = rnd.Next(0, 38);
+                counter++;
+                richTextBox1.AppendText($"{counter}) ");
+                //listBox1.Items.Add($"{counter - 1}) Movzu {randomNum}");
+                //listBox1.Items.Add($" ");
+
+            }
+        }
+        private void richTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                counter2++;
+                richTextBox2.AppendText($"{counter2}) ");
+            }
+        }
+
+
+        Random rnd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+
+            if (richTextBox1.Lines.Length > richTextBox2.Lines.Length)
+            {
+                MessageBox.Show("Təqdimat sayı tələbə sayından azdır!");
+            }
+
+            else if(!_isTrue)
+            {
+                List<string> StudentsList = new List<string>();
+                List<string> PresentationList = new List<string>();
+
+
+
+                for (int runs = 0; runs < richTextBox1.Lines.Length; runs++)
+                {
+                    StudentsList.Add(richTextBox1.Lines[runs]);
+                }
+
+                for (int j = 0; j < richTextBox2.Lines.Length; j++)
+                {
+                    PresentationList.Add(richTextBox2.Lines[j]);
+                }
+
+                string[] randomPresentation = PresentationList.OrderBy(x => rnd.Next()).ToArray();
+
+                for (int a = 0; a < richTextBox1.Lines.Length; a++)
+                {
+
+
+                    int indexOfBracket = richTextBox2.Lines[a].LastIndexOf(')');
+                    string randomPresentationItem = randomPresentation[a].Substring(indexOfBracket+1);
+                    if (randomPresentationItem.Contains(')'))
+                    {
+                       randomPresentationItem = randomPresentationItem.Replace(')',' ');
+
+                    }
+
+                    listBox1.Items.Add(StudentsList[a] + "  >>  " + randomPresentationItem);
+
+                }
+
+                _isTrue = true;
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            _isTrue = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (listBox1.Items.Count == 0)
+            {
+                MessageBox.Show("Nəticə Listi boşdur!");
+            }
+            else
+            {
+                foreach (string s in listBox1.Items)
+                {
+                    stringBuilder.AppendLine(s);
+                }
+                Clipboard.SetText(stringBuilder.ToString());
+                MessageBox.Show("Nəticələr Kopyalandı!");
+            }
+
+        }
+    }
+}
